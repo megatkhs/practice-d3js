475c752422f1fd9de16f876421aaa13a1363144a
megatkhs <tarodino0526@gmail.com>
2018-01-07 23:02:20 +0900

    追加 barChartを追加

--- js/svg_app.js
+++ js/svg_app.js
@@ -1,15 +1,12 @@
 'use strict';
 
 function D3Graph(setting) {
-  let dataset = function(){
-      let randData = [];
-      for(let i = 20; i > 0; i--){
-        randData.push(Math.random()*25);
-      }
-      return randData;
-    },
+  let dataset = [10, 20, 17, 20, 23, 15, 15, 10, 10, 16, 26, 13, 3],
       svg = d3.select('#field').append('svg').attr('width', setting.w).attr('height', setting.h),
-      circles = svg.selectAll('circle').data(dataset).enter().append('circle');
+      circles = svg.selectAll('circle').data(dataset).enter().append('circle'),
+      barChart = d3.select('#barChart').append('svg').attr('width', setting.w).attr('height', setting.h * 3),
+      bars = barChart.selectAll('rect').data(dataset).enter().append('rect'),
+      barPadding = 10;
 
   circles.attr('cx', function(d, i){
     return (i * 50) + 25;
@@ -23,6 +20,19 @@ function D3Graph(setting) {
   .attr('stroke-width', function(d){
     return d/2;
   });
+
+  bars.attr('x', function(d, i){
+    return i * (setting.w / dataset.length);
+  })
+  .attr('y', function(d){
+    return setting.h * 3 - d * 10;
+  })
+  .attr('width', setting.w / dataset.length - barPadding)
+  .attr('height', function(d){
+    return d * 10;
+  });
+
+  console.log(dataset);
 }
 
 let d3Graph = new D3Graph({
