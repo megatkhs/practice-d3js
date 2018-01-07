4cac6a59fd42c830f5c8442259803858edb90178
megatkhs <tarodino0526@gmail.com>
2018-01-07 22:17:47 +0900

    更新 乱数による円の描画がされるようになりました

--- js/svg_app.js
+++ js/svg_app.js
@@ -1,11 +1,31 @@
 'use strict';
 
 function D3Graph(setting) {
-  let svg = d3.select('#field').append('svg').attr('width', setting.w).attr('height', setting.h);
+  let dataset = function(){
+      let randData = [];
+      for(let i = 20; i > 0; i--){
+        randData.push(Math.random()*25);
+      }
+      return randData;
+    },
+      svg = d3.select('#field').append('svg').attr('width', setting.w).attr('height', setting.h),
+      circles = svg.selectAll('circle').data(dataset).enter().append('circle');
 
+  circles.attr('cx', function(d, i){
+    return (i * 50) + 25;
+  })
+  .attr('cy', setting.h / 2)
+  .attr('r', function(d){
+    return d;
+  })
+  .attr('fill', '#89f20f')
+  .attr('stroke', '#5ca804')
+  .attr('stroke-width', function(d){
+    return d/2;
+  });
 }
 
 let d3Graph = new D3Graph({
   w: 1280,
-  h: 720
+  h: 100
 });
