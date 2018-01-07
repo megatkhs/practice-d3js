c3a97e3ccaff12d895f4942473fd9bd80448aeea
megatkhs <tarodino0526@gmail.com>
2018-01-07 21:06:04 +0900

    追加 乱数によるグラフ作成とSVGへのリンクを追加

--- js/app.js
+++ js/app.js
@@ -1,24 +1,47 @@
 "use strict";
-d3.json("asset/data.json", function(json){
-  d3.select('#list').selectAll('li').data(json).enter().append('li').text(function(d){
-    return d['food'];
-  }).style('color', function(d){
-    if(d['delisiousness'] > 5){
-      return '#e0004a';
-    } else {
-      return '#0652e6';
-    }
+
+
+function D3Graph() {
+  d3.json("asset/data.json", function(json){
+    d3.select('#list').selectAll('li').data(json).enter().append('li').text(function(d){
+      return d['food'];
+    }).style('color', function(d){
+      if(d['delisiousness'] > 5){
+        return '#e0004a';
+      } else {
+        return '#0652e6';
+      }
+    });
+    d3.select('#barGraph').selectAll('div').data(json).enter().append('div').attr('class', 'bar')
+    .style('background', function(d){
+      if(d['delisiousness'] > 5){
+        return '#e0004a';
+      } else {
+        return '#0652e6';
+      }
+    })
+    .style('height', function(d){
+      return d['delisiousness'] * 20 + 'px';
+    });
+    console.log(d3.selectAll('p'));
   });
-  d3.select('#barGraph').selectAll('div').data(json).enter().append('div').attr('class', 'bar')
+
+  let randomDatas = [];
+  for(let i = 20; i > 0; i--){
+    randomDatas.push(Math.random()*10);
+  }
+  d3.select('#randomData').selectAll('div').data(randomDatas).enter().append('div').attr('class', 'bar')
   .style('background', function(d){
-    if(d['delisiousness'] > 5){
+    if(d > 5){
       return '#e0004a';
     } else {
       return '#0652e6';
     }
   })
   .style('height', function(d){
-    return d['delisiousness'] * 20 + 'px';
+    return d * 20 + 'px';
   });
-  console.log(d3.selectAll('p'));
-});
+
+}
+
+let d3Graph = new D3Graph();
